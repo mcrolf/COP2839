465bd33430a17fa9440367e98a79a0f25953905c
Michael Napoli <michael.napoli@mcrolf.com>
2023-10-13 15:43:55 -0400

    Initial Commit
    Tip Calculator Module 1

---
+++ TipCalculator/TipCalculator/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using TipCalculator.Models;
+
+namespace TipCalculator.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            ViewBag.Fifteen = 0;
+            ViewBag.Twenty = 0;
+            ViewBag.TwentyFive = 0;
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(TipCalculatorModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                ViewBag.Fifteen = model.CalcFifteenTip();
+                ViewBag.Twenty = model.CalcTwentyTip();
+                ViewBag.TwentyFive = model.CalcTwentyFiveTip();
+            }
+            else
+            {
+                ViewBag.Fifteen = 0;
+                ViewBag.Twenty = 0;
+                ViewBag.TwentyFive = 0;
+            }
+            return View(model);
+        }
+    }
+}
---
+++ TipCalculator/TipCalculator/Models/TipCalculatorModel.cs
@@ -0,0 +1,28 @@
+﻿namespace TipCalculator.Models
+{
+    public class TipCalculatorModel
+    {
+        public double MealCost { get; set; }
+        public double FifteenPercentTip { get; set; }
+        public double TwentyPercentTip { get; set; }
+        public double TwentyFivePercentTip { get; set; }
+
+        public double CalcFifteenTip()
+        {
+            FifteenPercentTip = MealCost * 0.15;
+            return FifteenPercentTip;
+        }
+
+        public double CalcTwentyTip()
+        {
+            TwentyPercentTip = MealCost * 0.2;
+            return TwentyPercentTip;
+        }
+
+        public double CalcTwentyFiveTip()
+        {
+            TwentyFivePercentTip = MealCost * 0.25;
+            return TwentyFivePercentTip;
+        }
+    }
+}
