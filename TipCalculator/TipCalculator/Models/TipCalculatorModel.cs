51af430fd140fc984b8b092f47a5f7d835573fd8
Michael Napoli <michael.napoli@mcrolf.com>
2023-10-13 16:19:03 -0400

    Input Validation added

--- TipCalculator/TipCalculator/Models/TipCalculatorModel.cs
+++ TipCalculator/TipCalculator/Models/TipCalculatorModel.cs
@@ -1,25 +1,33 @@
-﻿namespace TipCalculator.Models
+﻿using System.Runtime.CompilerServices;
+using System.ComponentModel.DataAnnotations;
+
+namespace TipCalculator.Models
 {
     public class TipCalculatorModel
     {
-        public double MealCost { get; set; }
-        public double FifteenPercentTip { get; set; }
-        public double TwentyPercentTip { get; set; }
-        public double TwentyFivePercentTip { get; set; }
+        [Required(ErrorMessage = "Please Enter an Amount...")]
+        [Range(0.5, 500, ErrorMessage = "Enter an amount between 0.5 and 500...")]
+        public double? MealCost { get; set; }
 
-        public double CalcFifteenTip()
+        public double? FifteenPercentTip { get; set; }
+
+        public double? TwentyPercentTip { get; set; }
+
+        public double? TwentyFivePercentTip { get; set; }
+
+        public double? CalcFifteenTip()
         {
             FifteenPercentTip = MealCost * 0.15;
             return FifteenPercentTip;
         }
 
-        public double CalcTwentyTip()
+        public double? CalcTwentyTip()
         {
             TwentyPercentTip = MealCost * 0.2;
             return TwentyPercentTip;
         }
 
-        public double CalcTwentyFiveTip()
+        public double? CalcTwentyFiveTip()
         {
             TwentyFivePercentTip = MealCost * 0.25;
             return TwentyFivePercentTip;
