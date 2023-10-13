c251d57b2051677d7b1cd75390f79d00935f3de7
Michael Napoli <michael.napoli@mcrolf.com>
2023-10-13 12:35:05 -0400

    Add project files.

---
+++ FutureValueCalculator/FutureValueCalculator/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FutureValueCalculator.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.Name = "James";
+            ViewBag.FV = 9999.99;
+            return View();
+        }
+    }
+}
---
+++ FutureValueCalculator/FutureValueCalculator/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FutureValueCalculator.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
